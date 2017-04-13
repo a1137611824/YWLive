bef5f33e9639b70c133973051fd68a0e6e361312
Mac <ruofeng@MacdeMacBook-Air.local>
2017-04-13 16:32:31 +0800

    firstCommit

---
+++ YWLive/Classes/Base/YWBaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  YWBaseViewController.swift
+//  YWLive
+//
+//  Created by Mac on 2017/4/13.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+import UIKit
+
+class YWBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+
+
+}
